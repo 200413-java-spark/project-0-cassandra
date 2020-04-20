ad6cbd84d0c6ad0023de8ec950d7431a636f6bac
Cassandra Terry <cassandra34terry@gmail.com>
2020-04-19 19:55:29 -0500

    refactoring parts of program to use objects

---
+++ src/main/java/com/github/wewe34/hangman/Hangman.java
@@ -0,0 +1,17 @@
+package com.github.wewe34.hangman;
+
+
+class Hangman {
+
+    public static void main(String args[]){
+
+        Play hangman = new Play();
+        hangman.playGame();
+    }
+}
+
+
+// Stick man symbols to use
+        //  0
+        // -|-
+        //  /\
--- src/main/java/com/github/wewe34/hangman/Play.java
+++ src/main/java/com/github/wewe34/hangman/Play.java
@@ -1,42 +1,41 @@
-package com.github.wewe34;
+package com.github.wewe34.hangman;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
-import javax.swing.text.WrappedPlainView;
 
-class Hangman {
+public class Play {
 
-    public static void main(String args[]){
 
-        String[] words = new String[]{"lightbulb", "pasta", "skirt", "zebra", "faith", "banana", "microsystem"};
+    public void playGame(){
+
         Scanner hangman = new Scanner(System.in);
-        Random rand = new Random();
+        WordGenerator word = new WordGenerator();
+        String wordToGuess = word.generateWord();
+        String[] correctGuessArray = word.generateBlankArray();
 
-        int wordIndex = rand.nextInt(4);
-        String wordToGuess = words[wordIndex];
+        // word to guess turned to an array
         char[] wordToGuessArray = wordToGuess.toCharArray();
+
+        // when to end game
         int guessesRemaining = 6;
         boolean win = false;
-        String[] correctGuessArray =  new String[wordToGuess.length()];
-
-        for (int i = 0; i < wordToGuess.length(); i++) {
-                correctGuessArray[i] = "_";
-        }
+        ArrayList<String> playerGuesses = new ArrayList<>();
 
         while (guessesRemaining > 0 && win == false) {
-            System.out.println(Arrays.toString(correctGuessArray));
-            System.out.println(wordToGuess);
+            System.out.println("\nWORD HAS " + wordToGuess.length() + " LETTERS.");
+            System.out.println("YOU HAVE  " + guessesRemaining + "  GUESSES REMAINING.");
+            System.out.println("MYSTERY WORD: " + Arrays.toString(correctGuessArray));
             System.out.println("Enter guess");
             String guess = hangman.nextLine().toLowerCase();
             char guessChar = guess.charAt(0);
             boolean correctGuess = wordToGuess.contains(guess);
+            playerGuesses.add(guess);
 
             if (correctGuess == true) {
                 //find the index of matching character
-                System.out.println("CORRECT");
+                System.out.println("\n\nCORRECT!");
                 for(int i = 0; i < wordToGuess.length(); i++){
                     if (wordToGuessArray[i] == guessChar){
                             correctGuessArray[i] = guess;
@@ -45,23 +44,17 @@ class Hangman {
                 boolean containsUnderScore = Arrays.stream(correctGuessArray).anyMatch("_"::equals);
                 if (containsUnderScore == false) {
                     win = true;
-                    System.out.println("YOU WON. THE WORD WAS " + wordToGuess + "!");
+                    System.out.println("\nYOU WON. THE WORD WAS " + wordToGuess.toUpperCase() + "!");
                 }
             }
             else {
+                System.out.println("\n\nWRONG!");
                 if(guessesRemaining == 1){
-                    System.out.println("SORRY. YOU HAVE NO MORE GUESSES LEFT. THE WORD WAS " + wordToGuess);
+                    System.out.println("\nSORRY. YOU HAVE NO MORE GUESSES LEFT. THE WORD WAS--- " + wordToGuess.toUpperCase());
                 }
                 guessesRemaining -= 1;
-                System.out.println("WRONG GUESS. YOU HAVE " + guessesRemaining + " GUESSES REMAINING.");
             }
+            System.out.println("YOUR GUESSES: " + Arrays.toString(playerGuesses.toArray()));
         }
-
     }
 }
-
-
-// Stick man symbols to use
-        //  0
-        // -|-
-        //  /\
